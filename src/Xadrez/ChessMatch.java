adfae02f72fda13cb641b5c1bc32b1079f21e26d
Artur Magalhães <artur.magalhaes@aluno.uece.br>
2019-04-23 16:49:41 -0300

    Adicionando a classe Rainha

--- src/Xadrez/ChessMatch.java
+++ src/Xadrez/ChessMatch.java
@@ -8,6 +8,7 @@ import Xadrez.piece.Bishop;
 import Xadrez.piece.King;
 import Xadrez.piece.Knight;
 import Xadrez.piece.Pawn;
+import Xadrez.piece.Queen;
 import Xadrez.piece.Rook;
 import tabuleiro.Board;
 import tabuleiro.Piece;
@@ -202,6 +203,7 @@ public class ChessMatch {
 		placeNewPiece('g', 1, new Knight(board, Color.WHITE));
 		placeNewPiece('c', 1, new Bishop(board, Color.WHITE));
 		placeNewPiece('f', 1, new Bishop(board, Color.WHITE));
+		placeNewPiece('d', 1, new Queen(board, Color.WHITE));
 		placeNewPiece('e', 1, new King(board, Color.WHITE));
 		placeNewPiece('a', 2, new Pawn(board, Color.WHITE));
 		placeNewPiece('b', 2, new Pawn(board, Color.WHITE));
@@ -218,6 +220,7 @@ public class ChessMatch {
 		placeNewPiece('g', 8, new Knight(board, Color.BLACK));
 		placeNewPiece('c', 8, new Bishop(board, Color.BLACK));
 		placeNewPiece('f', 8, new Bishop(board, Color.BLACK));
+		placeNewPiece('d', 8, new Queen(board, Color.BLACK));
 		placeNewPiece('e', 8, new King(board, Color.BLACK));
 		placeNewPiece('a', 7, new Pawn(board, Color.BLACK));
 		placeNewPiece('b', 7, new Pawn(board, Color.BLACK));
